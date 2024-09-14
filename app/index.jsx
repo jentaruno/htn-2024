6a940d2a7dbdde8582521f74348fd77a5700f99c
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-09-14 09:52:42 -0700

    Basic page routing

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <View>
+      <Link href="/camera">Camera</Link>
+      <Link href="/food-info">Food Info</Link>
+    </View>
+  );
+}
