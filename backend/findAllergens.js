8aeaac1e989b076851e9e11daad1ce6c351a9438
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-09-14 18:51:57 -0700

    Successful groq api call

---
+++ backend/findAllergens.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const Groq = require('groq-sdk');
+
+const apiKey = process.env.GROQ_API_KEY;
+const groq = new Groq({apiKey: apiKey});
+
+async function findAllergens(foodItem) {
+    const prompt = "give me a list of allergens for the food that the user is looking for. return only a list of allergens as a JSON array of strings, no need to respond with any additional information.";
+    const chatCompletion = await groq.chat.completions.create({
+        "messages": [
+            {
+                "role": "system",
+                "content": prompt
+            },
+            {
+                "role": "user",
+                "content": foodItem
+            }
+        ],
+        "model": "llama3-8b-8192",
+        "temperature": 1,
+        "max_tokens": 1024,
+        "top_p": 1,
+        "stream": true,
+        "stop": null
+    });
+
+    let result = "";
+    for await (const chunk of chatCompletion) {
+        result += chunk.choices[0]?.delta?.content || '';
+    }
+    return JSON.parse(result);
+}
+
+module.exports = findAllergens;
--- backend/foodInfoRouter.js
+++ backend/foodInfoRouter.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const findAllergens = require('./findAllergens');
 const router = express.Router();
 
 router.get("", async (req, res) => {
     try {
-        res.send("Bonsoir!");
+        let foodItems = [{
+            productName: "пшенная с маслом",
+            productNameTranslation: "millet with butter",
+            description: "millet porridge with butter",
+        }];
+        let result = await Promise.all(foodItems.map(async e => {
+            const allergens = await findAllergens(e.productName);
+            return {
+                ...e,
+                possibleAllergens: allergens
+            };
+        }));
+        res.send(result);
     } catch (e) {
         console.error('Error fetching data:', e);
         res.status(500).send('Error fetching data');
