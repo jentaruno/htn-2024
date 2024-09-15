f2687aad3e6efcbaf116a501c5412d429684ec42
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-09-14 20:22:50 -0700

    Fetch req in provider

--- frontend/data/MenuScanProvider.jsx
+++ frontend/data/MenuScanProvider.jsx
@@ -1,5 +1,6 @@
 import {createContext, useContext, useEffect, useState} from "react";
 
+const GET_INFO_API_LINK = "localhost:8000/"
 const MenuScanContext = createContext(undefined);
 
 export function useMenuScan () {
@@ -14,6 +15,44 @@ export function MenuScanProvider({children}) {
     const [isLoading, setIsLoading] = useState(false);
     const [menuImage, setMenuImage] = useState(null);
     const [foodInfo, setFoodInfo] = useState(null);
+
+    async function fetchFoodInfo(image) {
+        try {
+            const response = await fetch(
+                GET_INFO_API_LINK,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: image
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch food info");
+            }
+            const data = await response.json();
+            setFoodInfo(data);
+            console.log(data);
+        } catch (e) {
+            throw Error("Failed to fetch user data: ", e);
+        }
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+        const handleScanImage = async () => {
+            try {
+                await fetchFoodInfo(menuImage.base64);
+            } catch (e) {
+                console.error(e);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        handleScanImage();
+    }, [menuImage]);
 
     const value = {
         menuImage,
